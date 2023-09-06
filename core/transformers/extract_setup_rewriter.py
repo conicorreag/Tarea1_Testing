e7645c6106a1e5ea51d43bbc88591a0953a2fb26
conicorreag <conicorreag@uc.cl>
2023-09-06 20:30:24 -0300

    setup trans listo

--- core/transformers/extract_setup_rewriter.py
+++ core/transformers/extract_setup_rewriter.py
@@ -30,6 +30,7 @@ class DuplicatedSetupVisitor(NodeVisitor):
         return self.duplicated_lines
 
 
+
     def visit_FunctionDef(self, node):
         method_name = node.name
         method_lines = []
@@ -48,18 +49,23 @@ class ExtractSetupTransformer(NodeTransformer):
         super().__init__()
         self.duplicated_lines = []
         self.duplicated_variables = []
+        self.duplicated_values = []
         
     def visit_ClassDef(self, node):
+      
         self.duplicated_lines = DuplicatedSetupVisitor().visit(node)
-        print("lineas duplicadas")
-        for i in self.duplicated_lines:
-            print(i)
         self.generic_visit(node)
-        print("\n\ndespues generic")
-        print(ast.dump(node))
+        if self.duplicated_lines:
+            new_method = FunctionDef(name='setUp', 
+                                    args=arguments(posonlyargs=[], args=[arg(arg='self')], kwonlyargs=[], kw_defaults=[], defaults=[]),
+                                    body=[], decorator_list=[])
+            for i in range(0, len(self.duplicated_lines)):
+                line = Assign(targets=[Attribute(value=Name(id='self', ctx=Load()), attr=self.duplicated_variables[i], ctx=Store())], value=self.duplicated_values[i])
+                new_method.body.append(line)
 
-        new_method = FunctionDef(name='setUp', args=arguments(args=[], vararg=None, kwonlyargs=[], kw_defaults=[], kwarg=None, defaults=[]), body=[], decorator_list=[], returns=None)
-
+            node.body.insert(0, new_method)
+    
+        return node
 
 
     def visit_FunctionDef(self, node):
@@ -72,6 +78,8 @@ class ExtractSetupTransformer(NodeTransformer):
                 if isinstance(node.body[m], ast.Assign):
                     variable = node.body[m].targets[0].id
                     self.duplicated_variables.append(variable)
+                    value = node.body[m].value
+                    self.duplicated_values.append(value)
 
         for i in lines_remove:
             node.body.remove(i)
@@ -80,14 +88,17 @@ class ExtractSetupTransformer(NodeTransformer):
         return node
     
 
+
     def visit_Call(self, node):
         for i in range(0, len(node.args)):
             if isinstance(node.args[i], ast.Name):
                 if node.args[i].id in self.duplicated_variables:
                     node.args[i] = Attribute(value=Name(id='self', ctx=Load()), attr=node.args[i].id, ctx=Load())
-        print("-------holaaaaa---------")
-        print("\nnodo cambiando variables")
-        print(ast.dump(node))
+            elif isinstance(node.args[i], ast.Call):
+                if node.args[i].func.value.id in self.duplicated_variables:
+                    node.args[i] = Call(func=Attribute(value=Attribute(value=Name(id='self', ctx=Load()), attr=node.args[i].func.value.id, ctx=Load()), attr=node.args[i].func.attr, ctx=Load()), args=[], keywords=[])
+
+
         return node
         
 
@@ -99,6 +110,7 @@ class ExtractSetupCommand(RewriterCommand):
         # La funcion fix_missing_locations se utiliza para recorrer los nodos del AST y actualizar ciertos atributos
         # (e.g., número de línea) considerando ahora la modificacion
         new_tree = fix_missing_locations(ExtractSetupTransformer().visit(ast))
+        
         return new_tree
 
     @classmethod
