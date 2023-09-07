578a7c614fc44010c4ef1fde3ea4a9d75bb8fac9
Ignacia Correa <ignaciacorrea@uc.cl>
2023-09-07 00:30:46 -0300

    inline trasnform

--- core/transformers/inline_rewriter.py
+++ core/transformers/inline_rewriter.py
@@ -1,53 +1,74 @@
+from _ast import Call, FunctionDef, Module, Name
 from ast import *
+from typing import Any
 from core.rewriter import RewriterCommand
 
 
-# Este transformador busca todas las variables que se utilizaron una sola vez despues de ser inicializadas, posteriormente elimina estas variables sin modificar la logica del test. Para este efecto, reemplaza la variable a eliminar por la expresion utilizada para inicializar la misma.
-class InlineTransformer(NodeTransformer):
+class InlineVisitor(NodeVisitor):
     def __init__(self):
-        self.variables = {} # guarda en un diccionario las variables inicializadas con su valor
-        self.used_variables = {} # guarda en un diccionario las variables utilizadas con la cantidad de veces que se usa
+        self.variables = {} # Diccionario para variables inicializadas con sus valores
+        self.variable_line = {} # Diccionario para variables inicializadas con el valor de la linea en el que se inicializan
+        self.used_variables = {} # Diccionario para variables utilizadas con la cantidad de veces que se usan
 
     def visit_Assign(self, node: Assign):
+        self.generic_visit(node)
         if isinstance(node.targets[0], Name):
-            target_variable = node.targets[0].id
-            self.variables[target_variable] = node.value
-            print("Variable inicializada: ", target_variable, " = ", node.value)
-        NodeTransformer.generic_visit(self,node)
+            variable_name = node.targets[0].id
+            self.variables[variable_name] = node.value
+            self.variable_line[variable_name] = node.lineno
+            if isinstance(node.value, BinOp):
+                self._check_and_record_variables(node.value.left)
+                self._check_and_record_variables(node.value.right)
+
+    def visit_Call(self, node: Call):
+        self.generic_visit(node)
+        for arg in node.args:
+            self._check_and_record_variables(arg)
+
+    def _check_and_record_variables(self, variable_node):
+        if isinstance(variable_node, Name) and variable_node.id in self.variables:
+            self.used_variables[variable_node.id] = self.used_variables.get(variable_node.id, 0) + 1
+    
+    def visit_FunctionDef(self, node: FunctionDef):
+        self.generic_visit(node)
+        return [self.variables, self.used_variables, self.variable_line]
+
+
+
+class InlineTransformer(NodeTransformer):
+    def __init__(self):
+        super().__init__()
+        self.variables = {} # Diccionario para variables inicializadas con sus valores
+        self.used_variables = {} # Diccionario para variables utilizadas con la cantidad de veces que se usan
+        self.variable_line = {} # Diccionario para variables inicializadas con el valor de la linea en el que se inicializan
+
     
     def visit_Name(self, node: Name):
-        if isinstance(node.ctx, Load) and node.id in self.variables.keys():
-            self.used_variables[node.id] = self.used_variables.get(node.id, 0) + 1
-        NodeTransformer.generic_visit(self,node)
-    
-    def visit_Call(self, node: Call):
-        for arg in node.args:
-            if arg in self.variables.keys():
-                self.used_variables[arg] = self.used_variables.get(arg, 0) + 1
-        NodeTransformer.generic_visit(self,node)
-    
-    def optimize_unused_variables(self, node):
-        if isinstance(node, Module):
-            new_body = []
-            for stmt in node.body:
-                if isinstance(stmt, Assign):
-                    if isinstance(stmt.value, Name) and isinstance(stmt.value.ctx, Load):
-                        if stmt.value.id in self.used_variables and self.used_variables[stmt.value.id] == 1:
-                            value = self.variables[stmt.value.id]
-                            new_assign = Assign(targets=[Name(id=stmt.targets[0].id, ctx=Store())],
-                                                    value=value)
-                            new_body.append(new_assign)
-                            continue
-                new_body.append(stmt)
-            node.body = new_body
+        if self.variables and self:
+            if isinstance(node.ctx, Load) and node.id in self.used_variables:
+                if self.used_variables[node.id] == 1:
+                    return self.variables[node.id]
         return node
+    
+    def visit_Assign(self, node: Assign):
+        self.generic_visit(node)
+        if isinstance(node.targets[0], Name):
+            variable_name = node.targets[0].id
+            if variable_name in self.used_variables and self.used_variables[variable_name] == 1:
+                return None
+        return node
+    
+    def visit_FunctionDef(self, node: FunctionDef):
+        self.variables, self.used_variables, self.variable_line = InlineVisitor().visit(node)
+        self.generic_visit(node)
+        return node
+
 
 
 class InlineCommand(RewriterCommand):
     def apply(self, node):
-        visitor = InlineTransformer()
-        new_node = visitor.visit(node)
-        new_node = fix_missing_locations(new_node)
+        new_node = fix_missing_locations(InlineTransformer().visit(node))
+        print(dump(new_node))
         return new_node
     
     @classmethod
